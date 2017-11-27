5f2cfbd31011b34d9e618af29945808743e55c48
Kozlov Nikita <kozlovzxc@gmail.com>
2017-11-27 23:37:00 +0300

    add simple reflected xss sample

--- app/routes/samples.js
+++ app/routes/samples.js
@@ -6,4 +6,8 @@ samples.get('/domxss-hash', function(req, res, next) {
   res.render('samples/domxss-hash');
 });
 
+samples.get('/reflected-simple', function(req, res, next) {
+  res.render('samples/reflected-simple', { text: req.query.text})
+})
+
 module.exports = samples;
