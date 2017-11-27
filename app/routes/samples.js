9651b0b4e714d6733e08c78d2cceb1308a6f7d27
Kozlov Nikita <kozlovzxc@gmail.com>
2017-11-28 00:12:57 +0300

    add reflected xss via cookie, fix index page marings

--- app/routes/samples.js
+++ app/routes/samples.js
@@ -10,16 +10,34 @@ dom.get('/hash', function(req, res, next) {
 var reflected = express.Router();
 
 reflected.get('/simple', function(req, res, next) {
-  res.render('samples/reflected/simple', { text: req.query.text })
-})
+  res.render('samples/reflected/simple', { text: req.query.text });
+});
 
 reflected.get('/json', function(req, res, next) {
-  res.render('samples/reflected/json', { text: JSON.stringify(req.query.text) })
-})
+  res.render('samples/reflected/json', { text: JSON.stringify(req.query.text) });
+});
 
 reflected.get('/attribute', function(req, res, next) {
-  res.render('samples/reflected/attribute', { attribute: req.query.attribute })
-})
+  res.render('samples/reflected/attribute', { attribute: req.query.attribute });
+});
+
+reflected.get('/cookie', function(req, res, next) {
+  if (req.cookies.cookieValue === undefined) {
+    res.render('samples/reflected/cookie', { text: 'No cookie' });
+  } else {
+    res.render('samples/reflected/cookie', { text: req.cookies.cookieValue });
+  }
+});
+
+reflected.post('/cookie', function(req, res, next) {
+  if (req.body.cookieValue) {
+    res.cookie('cookieValue', req.body.cookieValue);
+    res.redirect(req.originalUrl);
+  } else {
+    res.redirect(req.originalUrl);
+  }
+
+});
 
 samples.use('/dom', dom);
 samples.use('/reflected', reflected);
