48e09b835d2b0fed42edba0f93667d2679d67c35
Kirill Skvoter <skvoter46@gmail.com>
2017-12-17 19:05:43 +0300

    added attribute and json models

--- app/routes/stored.js
+++ app/routes/stored.js
@@ -25,48 +25,48 @@ stored.post('/simple', (req, res) => {
     }))
     .catch((err) => res.status(500).json(err));
 });
-/*stored.get('/attribute', (req, res) => {*/
-  //db.post
-    //.all()
-    //.then((posts) => {
-      //res.render('samples/stored/attribute', {
-        //posts,
-      //});
-    //})
-    //.catch((err) => res.json(err));
-//});
-//stored.post('/attribute', (req, res) => {
-  //db.post
-    //.create({
-      //data: req.body.data,
-    //})
-    //.then(() => db.post.all())
-    //.then((posts) => res.render('samples/stored/attribute', {
-      //posts,
-    //}))
-    //.catch((err) => res.status(500).json(err));
-//});
-//stored.get('/json', (req, res) => {
-  //db.post
-    //.all()
-    //.then((posts) => {
-      //res.render('samples/stored/json', {
-        //posts,
-      //});
-    //})
-    //.catch((err) => res.json(err));
-//});
-//stored.post('/json', (req, res) => {
-  //db.post
-    //.create({
-      //data: req.body.data,
-    //})
-    //.then(() => db.post.all())
-    //.then((posts) => res.render('samples/stored/json', {
-      //posts,
-    //}))
-    //.catch((err) => res.status(500).json(err));
-//});
+stored.get('/attribute', (req, res) => {
+  db.attribute
+    .all()
+    .then((posts) => {
+      res.render('samples/stored/attribute', {
+        posts,
+      });
+    })
+    .catch((err) => res.json(err));
+});
+stored.post('/attribute', (req, res) => {
+  db.attribute
+    .create({
+      data: req.body.data,
+    })
+    .then(() => db.attribute.all())
+    .then((posts) => res.render('samples/stored/attribute', {
+      posts,
+    }))
+    .catch((err) => res.status(500).json(err));
+});
+stored.get('/json', (req, res) => {
+  db.json
+    .all()
+    .then((posts) => {
+      res.render('samples/stored/json', {
+        posts,
+      });
+    })
+    .catch((err) => res.json(err));
+});
+stored.post('/json', (req, res) => {
+  db.json
+    .create({
+      data: req.body.data,
+    })
+    .then(() => db.json.all())
+    .then((posts) => res.render('samples/stored/json', {
+      posts,
+    }))
+    .catch((err) => res.status(500).json(err));
+}); 
 
 
 module.exports = stored;
