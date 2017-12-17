a94bcc700ace493078b8891cea882425d2aa0443
Kozlov Nikita <kozlovzxc@gmail.com>
2017-12-17 16:33:47 +0300

    remove samples folder from views

--- app/routes/reflected.js
+++ app/routes/reflected.js
@@ -4,26 +4,26 @@ const express = require('express');
 const reflected = express.Router();
 
 reflected.get('/simple', (req, res) => {
-  res.render('samples/reflected/simple', { text: req.query.text });
+  res.render('reflected/simple', { text: req.query.text });
 });
 
 reflected.get('/script', (req, res) => {
-  res.render('samples/reflected/inscript', { value: req.query.value });
+  res.render('reflected/inscript', { value: req.query.value });
 });
 
 reflected.get('/json', (req, res) => {
-  res.render('samples/reflected/json', { text: JSON.stringify(req.query.text) });
+  res.render('reflected/json', { text: JSON.stringify(req.query.text) });
 });
 
 reflected.get('/attribute', (req, res) => {
-  res.render('samples/reflected/attribute', { attribute: req.query.attribute });
+  res.render('reflected/attribute', { attribute: req.query.attribute });
 });
 
 reflected.get('/cookie', (req, res) => {
   if (req.cookies.cookieValue === undefined) {
-    res.render('samples/reflected/cookie', { text: 'No cookie' });
+    res.render('reflected/cookie', { text: 'No cookie' });
   } else {
-    res.render('samples/reflected/cookie', { text: req.cookies.cookieValue });
+    res.render('reflected/cookie', { text: req.cookies.cookieValue });
   }
 });
 
